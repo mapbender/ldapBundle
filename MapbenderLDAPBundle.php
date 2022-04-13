2c79612669ecf1e7a1160615bb98d7c9a40da94c
werrolf <rolf.neuberger@wheregroup.com>
2022-04-13 14:15:07 +0200

    Drop pure config loading extension, use container build

--- MapbenderLDAPBundle.php
+++ MapbenderLDAPBundle.php
@@ -2,6 +2,9 @@
 
 namespace Mapbender\LDAPBundle;
 
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Config\Resource\FileResource;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Mapbender\LDAPBundle\DependencyInjection\Compiler\OverwriteIdentitiesProviderPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
@@ -14,9 +17,18 @@ class MapbenderLDAPBundle extends Bundle
     public function build(ContainerBuilder $container)
     {
         parent::build($container);
+        $configLocator = new FileLocator(__DIR__ . '/Resources/config');
+        $loader = new YamlFileLoader($container, $configLocator);
+        $loader->load('services.yml');
+        $container->addResource(new FileResource($configLocator->locate('services.yml')));
+
         $extension = $container->getExtension('security');
         $container->addCompilerPass(new OverwriteIdentitiesProviderPass());
         $extension->addSecurityListenerFactory(new MapbenderLDAPLoginFactory());
     }
 
+    public function getContainerExtension()
+    {
+        return null;
+    }
 }
