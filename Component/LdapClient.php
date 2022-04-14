f82c1c8cff91a1002d4a1e9824e3ec7444bc29a7
werrolf <rolf.neuberger@wheregroup.com>
2022-04-14 11:29:16 +0200

    Bridge / alias ldap client into fom.ldap_client for tls / ssl support

--- Component/LdapClient.php
+++ Component/LdapClient.php
@@ -5,14 +5,15 @@ namespace Mapbender\LDAPBundle\Component;
 
 
 use Symfony\Component\Ldap\Adapter\ExtLdap\Adapter;
+use Symfony\Component\Ldap\Adapter\QueryInterface;
 use Symfony\Component\Ldap\LdapInterface;
 
-class LdapClient implements LdapInterface
+class LdapClient extends \FOM\UserBundle\Component\Ldap\Client implements LdapInterface
 {
     /** @var \Symfony\Component\Ldap\Adapter\AdapterInterface */
     protected $adapter;
 
-    public function __construct($host, $port, $version, $useSsl, $useTls)
+    public function __construct($host, $port, $version, $useSsl, $useTls, $bindDn, $bindPassword)
     {
         // Create an ldap adapter (Symfony >= 3)
         /** @see \Symfony\Component\Ldap\Adapter\AbstractConnection::configureOptions for possible options */
@@ -28,6 +29,7 @@ class LdapClient implements LdapInterface
             $adapterOptions['encryption'] = 'tls';
         }
         $this->adapter = new Adapter($adapterOptions);
+        parent::__construct($host, $port, $version, $bindDn, $bindPassword);
     }
 
     public function bind($dn = null, $password = null)
@@ -48,5 +50,28 @@ class LdapClient implements LdapInterface
     public function escape($subject, $ignore = '', $flags = 0)
     {
         return $this->adapter->escape($subject, $ignore, $flags);
+    }
+
+    /**
+     *  FOM client compatibility
+     * @see \FOM\UserBundle\Component\Ldap\Client::getObjects
+     *
+     * @param $baseDn
+     * @param $filter
+     * @return \array[][]
+     */
+    public function getObjects($baseDn, $filter)
+    {
+        if (!$this->adapter->getConnection()->isBound()) {
+            $this->adapter->getConnection()->bind($this->bindDn, $this->bindPassword);
+        }
+        $query = $this->adapter->createQuery($baseDn, $filter, array(
+            'scope' => QueryInterface::SCOPE_ONE,
+        ));
+        $records = array();
+        foreach ($query->execute() as $entry) {
+            $records[] = $entry->getAttributes();
+        }
+        return $records;
     }
 }
